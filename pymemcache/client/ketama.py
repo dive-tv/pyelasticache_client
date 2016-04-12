d35443e2d1554e706e7ab9a53eb7ec5c010047be
Guillermo Menéndez <guillermo.menendez@touchvie.com>
2016-04-12 19:06:40 +0200

    Added seed to ketama hash

--- pymemcache/client/ketama.py
+++ pymemcache/client/ketama.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 import hashlib
 
-def ketama_hash(data):
+def ketama(data, h=0):
 
     m = hashlib.md5()
     m.update(data.encode('utf-8'))
     b_key = m.digest()
 
     if (type(b_key[0]) is int):
-        rv = ((b_key[3] & 0xFF) << 24) | ((b_key[2] & 0xFF) << 16) | ((b_key[1] & 0xFF) << 8) | (b_key[0] & 0xFF)
+        rv = ((b_key[3 + h * 4] & 0xFF) << 24) | ((b_key[2 + h * 4] & 0xFF) << 16) | ((b_key[1 + h * 4] & 0xFF) << 8) | (b_key[0 + h * 4] & 0xFF)
     else:
-        rv = ((ord(b_key[3]) & 0xFF) << 24) | ((ord(b_key[2]) & 0xFF) << 16) | ((ord(b_key[1]) & 0xFF) << 8) | (ord(b_key[0]) & 0xFF)
+        rv = ((ord(b_key[3 + h * 4]) & 0xFF) << 24) | ((ord(b_key[2 + h * 4]) & 0xFF) << 16) | ((ord(b_key[1 + h * 4]) & 0xFF) << 8) | (ord(b_key[0 + h * 4]) & 0xFF)
         
-    return rv& 0xFFFFFFFF
+    return rv & 0xFFFFFFFF
